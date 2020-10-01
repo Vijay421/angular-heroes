7cd1bd6ac54f3e2ef3f9c2d05515e436914baf59
Vijay <vijay99sankar@gmail.com>
2020-10-01 21:51:35 +0200

    commented unnecessary code

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit /*, Input*/ } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import Hero from '../hero';
@@ -12,7 +12,7 @@ import HeroService from '../hero.service';
 })
 export class HeroDetailComponent implements OnInit {
 
-	@Input() hero: Hero;
+	/*@Input()*/ hero: Hero;
 
 	constructor(
 		private route: ActivatedRoute,
